7f382773b55ff794f56bf0ae9322769411e8e9f1
Sarthak-Sentieo <sarthak.nirala@sentieo.com>
2022-01-31 21:18:28 +0530

    Demo Data

---
+++ src/Components/Right_Side/SharedCalender.js
@@ -0,0 +1,22 @@
+import React from 'react'
+
+export const SharedCalender =[
+    {
+        title:"TouchBase with PM",
+        date: "15th Jan'21. 10:00-10:30 AM"
+    },
+    {
+        title:"TouchBase with PM",
+        date: "15th Jan'21. 10:00-10:30 AM"
+    },
+    {
+        title:"TouchBase with PM",
+        date: "15th Jan'21. 10:00-10:30 AM"
+    },
+    {
+        title:"TouchBase with PM",
+        date: "15th Jan'21. 10:00-10:30 AM"
+    },
+   
+];
+
---
+++ src/Components/Right_Side/SharedNotes.js
@@ -0,0 +1,26 @@
+import React from 'react'
+
+export const SharedNotes =[
+    {
+        title:"Meeting Notes From Portfolio Class A Review",
+        name:"Anurag",
+        date: "15 Jan'21 "
+    },
+    {
+        title:"Equity Research Team Morning Review",
+        name:"John Doe",
+        date: "15 Jan'21 "
+    },
+    {
+        title:"Equity Research Team Morning Review",
+        name:"John Doe",
+        date: "15 Jan'21 "
+    },
+    {
+        title:"Equity Research Team Morning Review",
+        name:"Anurag",
+        date: "15 Jan'21 "
+    },
+   
+];
+
